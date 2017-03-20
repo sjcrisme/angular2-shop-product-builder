d61c0a6a1faf5f5d8dd39fc285c465c6b4b195e8
Serhii Verbitskiy <sjcris.me@gmail.com>
2017-03-20 22:11:18 +0200

    first init

---
+++ src/app/items-list/items-list.component.ts
@@ -0,0 +1,10 @@
+inport { Component } from '@agular/core'
+
+
+@Comment({
+    selector:'hg-items-list',
+    template:`
+        <div> item List</div>
+    `
+})
+export class ItemsListComponent{ }
